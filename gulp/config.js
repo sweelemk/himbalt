d723f07050508a3316c86515bc1067a3e922adbf
sweelemk <sweelemk@gmail.com>
2016-08-17 20:14:47 +0300

    pug

---
+++ gulp/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  src : {
+    root    : 'src/',
+    pug    : 'src/pug',
+    sass    : 'src/sass/',
+    js      : 'src/js/',
+    img     : 'src/img/',
+    helpers : 'gulp/helpers/'
+  },
+  dest:{
+    root    : 'bitrix/templates/main/',
+    css     : 'bitrix/templates/main/css/',
+    html    : 'bitrix/templates/main/',
+    js      : 'bitrix/templates/main/js/',
+    img     : 'bitrix/templates/main/img/'
+  },
+};
